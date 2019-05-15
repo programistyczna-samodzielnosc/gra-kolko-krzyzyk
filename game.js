74d7666b64978cd0953f81586f26dc41d7bd8c3c
Cyprian Gepfert <cgepfert@gmail.com>
2019-05-15 23:15:43 +0200

    blokada niepoprawnych ruchow; poprawne wyswietlanie biezacego gracza

--- game.js
+++ game.js
@@ -24,39 +24,40 @@ function game() {
 
     elementBoard.addEventListener('click', move)
 
-    
-
     function displayCurrentPlayer() {
         let status = document.querySelector('.status');
         status.innerText = `Bieżący gracz: ${currentPlayer.name}`;
+    }
+
+    function move(event) {
+        if (isCellEmpty(event.target) && isTargetCorrect(event.target)) {
+            event.target.classList.add(`board__cell--${currentPlayer.name}`);
+            event.target.classList.remove(`board__cell--${opponentPlayer.name}`);
+            moveCounter++;
+            opponentPlayer = players[((Math.abs(moveCounter - 1)) % 2)];
+            currentPlayer = players[(moveCounter % 2)];
+            displayCurrentPlayer();
+        }
+
+        // updateBoard();
+        // checkWin();
     }
 
     function isTargetCorrect(target) {
         return target.classList.contains("board__cell");
     }
 
-
-    function move(event) {
-        opponentPlayer = players[((Math.abs(moveCounter - 1)) % 2)];
-        currentPlayer = players[(moveCounter % 2)];
-
-        if (isTargetCorrect(event.target)) {
-            event.target.classList.add(`board__cell--${currentPlayer.name}`);
-            event.target.classList.remove(`board__cell--${opponentPlayer.name}`);
-            moveCounter++;
-            displayCurrentPlayer();
-        }
-
-
-        // updateBoard();
-        // checkWin();
-
+    function isTargetKrzyzyk(target) {
+        return target.classList.contains("board__cell--krzyzyk");
     }
 
+    function isTargetKolko(target) {
+        return target.classList.contains("board__cell--kolko");
+    }
 
-
-
-
+    function isCellEmpty(target) {
+        return !isTargetKolko(target) && !isTargetKrzyzyk(target);
+    }
 }
 
 window.onload = game;
